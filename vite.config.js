c934309e960712c96cc8ff3dcc4042d8099332d4
Himanshu rana <137643570+himanshurana18@users.noreply.github.com>
2025-09-26 22:32:58 +0530

    new one

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// 👇 Convert ESM URL to directory path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "src"),
       "@components": path.resolve(__dirname, "src/components"),
       "@pages": path.resolve(__dirname, "src/pages"),
       "@lib": path.resolve(__dirname, "src/lib"),
